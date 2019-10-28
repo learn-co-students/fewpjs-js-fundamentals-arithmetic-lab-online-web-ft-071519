b3b60d1b72e749552428e5de3fa548ab2a72c638
Audrea Cook <audreacwork@gmail.com>
2019-10-28 10:42:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let random = parseInt(Number.random + 1);
+
+const mod = 24 % 5;
+
+const max = Math.max(2, 15, 20);
